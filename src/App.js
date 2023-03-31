6e3606d55de6d0d8d1834c67b90c35e36ccd9b10
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-03-31 16:52:40 +0900

    230331

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, {css} from "styled-components"; // 효과를 여러줄 넣을 경우 {css} 작성
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || "black"};
+  ${props => props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+      `
+  };
+  border-radius: 50%;
+`; // styled.div의 경우 ()말고 ``안에 내용들을 넣어준다.
+// background: red;
+
+// px - 고정값
+// 비율값들
+// em - font size: 10px일 경우 2em = 10px * 2
+// 2rem - font size, r = root = html태그(16px)의 * 2
+
+function App(){
+  return <Circle color="red" huge />; // 컴포넌트 자체에 설정된 값들을 props로 불러올 수 있다.
+  // return <Circle/>
 }
 
 export default App;
